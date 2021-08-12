7dc97b8ecac6afcaca55caabefba807c84b5a75b
Dokyung Lee <dkayleeee2@gmail.com>
2021-08-12 11:23:24 +0900

    Add input validate

--- 04BlogPost/ReduxSimpleStarter/src/actions/index.js
+++ 04BlogPost/ReduxSimpleStarter/src/actions/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 // action type
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
+
 // root url for API
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=ehrudWkd';
@@ -13,5 +15,15 @@ export function fetchPosts(){
     return {
         type: FETCH_POSTS,
         payload: request
+    };
+}
+
+// form -> property -> endpoint
+export function createPost(props) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+    return {
+        type: CREATE_POST,
+        patload: request
     };
 }
--- 04BlogPost/ReduxSimpleStarter/src/components/posts_new.js
+++ 04BlogPost/ReduxSimpleStarter/src/components/posts_new.js
@@ -1,8 +1,10 @@
+import { values } from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
-import PropTypes from 'prop-types';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
+
     render() {
 
         // const title = this.props.fields.title;
@@ -10,21 +12,31 @@ class PostsNew extends Component {
         console.log(title);
 
         return (
-            <form onSubmit={handleSubmit}>
+            // an action creator (handleSubmit)
+            <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create A New Post</h3>
-                <div className="form-group">
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
-                    <input type="text" className="form-control"/>
+                    <input type="text" className="form-control" {...title} />
+                    <div className="text-help">
+                        {title.touched ? title.error : ''}
+                    </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
-                    <input type="text" className="form-control"/>
+                    <input type="text" className="form-control" {...categories} />
+                    <div className="text-help">
+                        {categories.touched ? categories.error : ''}
+                    </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
-                    <textarea type="text" className="form-control"/>
+                    <textarea type="text" className="form-control" {...content} />
+                    <div className="text-help">
+                        {content.touched ? content.error : ''}
+                    </div>
                 </div>
 
                 <button type="submit" className="btn btn-primary">Submit</button>
@@ -33,13 +45,35 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = 'Enter a username';
+    }
+    if (!values.categories) {
+        errors.categories = 'Enter a categories';
+    }
+    if (!values.content) {
+        errors.content = 'Enter a content';
+    }
+
+    return errors;
+}
+
+
+
 // 리덕스 폼 구성 전달
+// connet: 1st argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps 
 export default reduxForm({
     // form key
     form: 'PostsNewForm',
     // form을 가지고 있는 field 선언
-    fields: ['title', 'categories', 'content']
-}) (PostsNew);
+    fields: ['title', 'categories', 'content'],
+    // form 검증
+    validate
+}, null, { createPost }) (PostsNew);
 
 // user types something in...record it on application state
 // state === {
