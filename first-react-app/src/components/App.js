081d432495b48acb1e599c850bc0116a1265e1fd
Dokyung Lee <dkayleeee2@gmail.com>
2021-08-13 18:20:37 +0900

    React Web 01

--- first-react-app/src/components/App.js
+++ first-react-app/src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 // import './css/App.css';
 import Home from './Home';
 import About from './About';
